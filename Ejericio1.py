62546402e52d1ce55d8ce8ff27d9ecaff7c813e0
JeisonOlaya <75368622+JeisonOlaya@users.noreply.github.com>
2025-04-21 18:48:28 -0500

    Add files via upload

---
+++ Ejericio1.py
@@ -0,0 +1,13 @@
+nombre = input("por favor dame tu nombre: ")
+apellido = input("por favor ingrese su apellido: ")
+edad = int (input("por favor ingrese su edad: "))
+print(nombre,apellido)
+print(edad)
+
+if edad>=18:
+    print("es mayor de edad")
+else: 
+    print("es menor de edad")
+    
+ 
+

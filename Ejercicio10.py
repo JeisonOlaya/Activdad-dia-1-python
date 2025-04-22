b34de5011bf74f46cf1cb5bb5daf3c0e813dee13
JeisonOlaya <75368622+JeisonOlaya@users.noreply.github.com>
2025-04-22 18:35:20 -0500

    Add files via upload

---
+++ Ejercicio10.py
@@ -0,0 +1,8 @@
+#Pide un número al usuario. Di si está dentro del rango de 1 a 10 (inclusive).
+
+Numero = int (input("Ingrese por favor un numero del 1 al 10: "))
+
+if Numero >=1 and Numero <=10:
+    print("su numero esta dentro del rango")
+else:
+    print("numero incorrecto")

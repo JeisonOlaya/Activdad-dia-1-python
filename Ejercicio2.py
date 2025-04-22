ed30bcb61496cf047c0447905ab63769e1b7601c
JeisonOlaya <75368622+JeisonOlaya@users.noreply.github.com>
2025-04-21 19:24:31 -0500

    Add files via upload

---
+++ Ejercicio2.py
@@ -0,0 +1,11 @@
+#Pide un número al usuario. Di si es positivo, negativo o si es cero.
+
+numero = int(input("por favor ingrese un numero: "))
+
+if numero>0:
+    input("el numero es positivo")
+elif numero<0:
+    input("el numero es negativo")
+elif numero == 0:
+    input("el numero es cero")
+       

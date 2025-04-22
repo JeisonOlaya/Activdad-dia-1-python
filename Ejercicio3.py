43cb5062b6f92f5b55c082e84f40fe17b88ec22a
JeisonOlaya <75368622+JeisonOlaya@users.noreply.github.com>
2025-04-21 19:46:18 -0500

    Add files via upload

---
+++ Ejercicio3.py
@@ -0,0 +1,7 @@
+#Pide un número entero. Indica si es par o impar.
+numero = int(input("digite un numero: "))
+
+if numero % 2 == 0:
+    print("el numero es par")
+else:
+    print("el numero es impar")
